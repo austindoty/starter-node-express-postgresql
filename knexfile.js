7a805ca1b7b8211da5ee2c34777c088beb289a8d
Austin Doty <asrdoty@gmail.com>
2023-11-14 11:30:35 -0500

    5.6.5

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,9 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   production: {
